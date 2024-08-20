dbdf198f285386947d5dd109bb4776a8f0db1e52
Raman Soni <69788442+RmnSoni@users.noreply.github.com>
2024-08-20 17:24:59 +0530

    Initial Commit

---
+++ SudokuApp/SudokuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuAppApp.swift
+//  SudokuApp
+//
+//  Created by Raman Soni on 20/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SudokuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
